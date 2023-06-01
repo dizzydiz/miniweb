1f57dc7af050b033134eacca4474db149faf5ec8
Stas Badaev <stas@cylus.com>
2023-06-01 21:19:40 +0300

    wip

---
+++ readme.md
@@ -0,0 +1,2 @@
+https://dhghomon.github.io/easy_rust/Chapter_10.html
+https://blog.logrocket.com/optimizing-rust-code-with-closures-environment-capturing/
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,10 +18,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, file_path) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, file_path) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     write_respons(&mut stream, status_line, file_path)
@@ -30,5 +35,5 @@ fn write_respons(stream: &mut TcpStream, status_line: &str, file_path: &str) {
 
     let content_length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
-    stream.write(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
 }
